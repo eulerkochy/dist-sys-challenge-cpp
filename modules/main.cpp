c1e86bab80031f588bd0300089ea27dec64398ae
Koustav Chowdhury <kc99.kol@gmail.com>
2025-12-28 20:22:22 +0530

    experiment: compile modules code

---
+++ modules/main.cpp
@@ -0,0 +1,10 @@
+import std;
+import math;
+
+int main() {
+  std::println("2 + 3 = {}", add(2, 3));
+  std::println("4 * 5 = {}", multiply(4, 5));
+  std::string_view str = "Hello, world!";
+  std::println("String: {}", str);
+  return 0;
+}
---
+++ modules/math.cppm
@@ -0,0 +1,5 @@
+export module math;
+
+export int add(int a, int b) { return a + b; }
+
+export int multiply(int a, int b) { return a * b; }
